b327ae29fa5260158d58466bcdf34f16cd09ac01
Bob <bob1993.dev@gmail.com>
2015-06-10 13:50:40 +0000

    添加了基于volley的ImageLoading

---
+++ app/src/main/java/com/bob/xtb/util/ImageLoading.java
@@ -0,0 +1,56 @@
+package com.bob.xtb.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageLoader.*;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by bob on 15-6-10.
+ */
+public class ImageLoading {
+
+    private ImageLoader imageLoader;//图片加载器
+    private static int maxSize= 10 * 1024 * 1024;//10mb
+    private static ImageLoading imageLoading;
+
+    private ImageLoading(ImageLoader imageLoader){
+        this.imageLoader= imageLoader;
+    }
+
+    public synchronized static ImageLoading getInstance(Context context){
+        if (imageLoading == null) {
+            imageLoading = new ImageLoading(new ImageLoader(Volley.newRequestQueue(context), new ImageCache() {
+
+                LruCache<String, Bitmap> mCache= new LruCache<String,Bitmap>(maxSize){//缓存总大小
+                    @Override//匿名内部类继承LruCache类，即缓存池
+                    protected int sizeOf(String key, Bitmap value) {
+                        return value.getRowBytes()*value.getHeight();//返回图片大小
+                    }
+                };
+
+                public Bitmap getBitmap(String url) {
+                    return mCache.get(url);
+                }
+
+                @Override
+                public void putBitmap(String url, Bitmap bitmap) {
+                    mCache.put(url, bitmap);
+                }
+            }));
+        }
+        return imageLoading;
+    }
+
+    public boolean loadImage(String url, ImageListener listener){
+        try {
+            imageLoader.get(url, listener);
+        }catch (Exception e){
+            return false;
+        }
+        return true;//加载成功
+    }
+}
